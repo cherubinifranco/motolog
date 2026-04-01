ec798ba059c459dbe05d68f4d2f3afa57872afaa
cherubinifranco <cherubini.franco@hotmail.com>
2026-04-01 06:36:55 -0300

    feat: db first config

---
+++ database/db.ts
@@ -0,0 +1,35 @@
+import * as SQLite from "expo-sqlite";
+
+export const db = await SQLite.openDatabaseAsync("motologDB");
+
+await db.execAsync(`
+  PRAGMA journal_mode = WAL;
+  PRAGMA foreign_keys = ON;
+
+  CREATE TABLE IF NOT EXISTS bikes (
+    id INTEGER PRIMARY KEY NOT NULL, 
+    brand TEXT NOT NULL, 
+    model TEXT NOT NULL, 
+    year INTEGER NOT NULL, 
+    currentKm INTEGER DEFAULT 0
+  );
+
+  CREATE TABLE IF NOT EXISTS services (
+    id INTEGER PRIMARY KEY NOT NULL, 
+    changeEvery INTEGER NOT NULL, 
+    title TEXT NOT NULL, 
+    icon TEXT NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS service_logs (
+    id INTEGER PRIMARY KEY NOT NULL, 
+    bikeId INTEGER NOT NULL, 
+    serviceId INTEGER NOT NULL,
+    mileage INTEGER NOT NULL, 
+    date TEXT NOT NULL, 
+    cost INTEGER DEFAULT 0, 
+    note TEXT,
+    FOREIGN KEY (bikeId) REFERENCES bikes(id)
+    FOREIGN KEY (serviceId) REFERENCES services(id)
+  );
+`);
