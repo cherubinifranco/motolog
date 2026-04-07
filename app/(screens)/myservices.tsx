f9c79b3ce04d3f4e0e3981a0cf8ea7b310dd155f
cherubinifranco <cherubini.franco@hotmail.com>
2026-04-07 18:02:16 -0300

    feat: added services as items

--- app/(screens)/myservices.tsx
+++ app/(screens)/myservices.tsx
@@ -1,21 +1,33 @@
 import ExternalLink from "@/components/ui/ExternalLink";
+import { ItemWithIcon } from "@/components/ui/ItemWithIcon";
 import { useServiceContext } from "@/context/ServiceContext";
-import { ScrollView, Text, View } from "react-native";
+import { Stack } from "expo-router";
+import { ScrollView } from "react-native";
 
 export default function MyServiceScreen() {
   const { services } = useServiceContext();
+
   return (
-    <ScrollView style={{ padding: 16 }}>
-      {services.map((el, index) => (
-        <View key={index}>
-          <Text>{el.title}</Text>
-        </View>
-      ))}
-      <ExternalLink
-        text="Crear otro servicio"
-        icon="add"
-        href="../newServiceModal"
-      />
-    </ScrollView>
+    <>
+      <Stack.Screen options={{ title: "Servicios" }} />
+
+      <ScrollView style={{ padding: 16 }}>
+        {services.map((el, index) => (
+          <ItemWithIcon
+            title={el.title}
+            subtitle={`Cada ${el.changeEvery} km`}
+            key={index}
+            icon={el.icon}
+            iconColor="orange"
+          />
+        ))}
+
+        <ExternalLink
+          text="Crear otro servicio"
+          icon="add"
+          href="../newServiceModal"
+        />
+      </ScrollView>
+    </>
   );
 }
